74477b25c4c87b7adc3df03fab1d45af9c0780fa
PascalDutlyCsBe <43207179+PascalDutlyCsBe@users.noreply.github.com>
2019-04-03 17:12:40 +0200

    Audio upload

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var multer = require("multer");
 const ejs = require('ejs');
 const fs = require('fs');
 var fluent_ffmpeg = require("fluent-ffmpeg");
+
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads/');
@@ -21,14 +22,23 @@ var storageVideo = multer.diskStorage({
     }
 });
 
+var storageAudio = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploadAudios/');
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.originalname);
+    }
+});
+
 var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('uploads'));
 var mergedVideo = fluent_ffmpeg();
 
-
 var upload = multer({ storage: storage });
 var uploadVideos = multer({ storage: storageVideo });
+var uploadAudios = multer({ storage: storageAudio });
 
 var gm = require("gm").subClass({
     imageMagick: true
@@ -92,10 +102,24 @@ app.post('/api/videos', uploadVideos.array('video'), function(req,res){
     res.status(200).json({success: true});
 });
 
+app.post('/api/audio', uploadAudios.array('audio'), function(req,res){
+    console.log("Der Name von BEIDEN Dateien lautet: " + req.body.audioName);
+    var mimetype0 = req.files[0].originalname.split('.')[1];
+    var mimetype1 = req.files[1].originalname.split('.')[1];
+    fs.rename(req.files[1].path, 'uploadAudios\\' + req.body.audioName + "." + mimetype1, function(err){
+        if ( err ) console.log('ERROR: ' + err);
+    });
+    fs.rename(req.files[0].path, 'uploadAudios\\' + req.body.audioName + "." + mimetype0, function(err){
+        if ( err ) console.log('ERROR: ' + err);
+    });
+    res.send("audio: uploadAudios\\" + req.body.audioName + "." + mimetype0 + " vtt: uploadAudios\\" + req.body.audioName + "." + mimetype1);
+});
+
 app.use('/files', express.static('uploads'));
 app.use('/js', express.static('js'));
 app.use('/assets', express.static('assets'));
 app.use('/css', express.static('css'));
+app.use('/uploadAudios', express.static('uploadAudios'));
 
 app.listen(process.env.PORT || 4000, function () {
     console.log('Your node js server is running on ' + process.env.PORT);
@@ -107,9 +131,11 @@ app.get('/', function (req, res) {
     res.render("\index.ejs");
 });
 
-app.get('/video_manager', function (req,res){
-    res.render("videoManager.ejs");
+app.get('/audio-manager', function (req, res) {
+    res.render("\audioManager.ejs");
 });
+
+app.get("/audio-player", (req,res) => res.render("\play_audio.ejs", {data: req.query.audioName}));
 
 app.get("/play_video", (req,res) => res.render("\play_video.ejs", {data: req.query.videoName}));
 
